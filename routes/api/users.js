40f22a102a7bde6a42b70c02806bda101de219a8
Michael McFarland <gardengrove422@gmail.com>
2018-05-16 16:32:45 -0400

    MongoDB and Postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -29,6 +30,17 @@ router.post("/register", (req, res) => {
         email: req.body.email,
         avatar: avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
