46c89fd909e6353d5b2f9b46bcc73e70861a29b5
Michael McFarland <gardengrove422@gmail.com>
2018-05-16 15:06:32 -0400

    Models

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access   Public
+
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
 module.exports = router;
